470496b62736d4285b3e1af5394a19010e27e914
Tosin Ezekiel <23019300+Tosin8@users.noreply.github.com>
2023-07-02 20:55:50 +0100

    Updating main with the page default

---
+++ lib/screens/shoe_category/shoe_category.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class ShoeCategory extends StatefulWidget {
+  const ShoeCategory({super.key});
+
+  @override
+  State<ShoeCategory> createState() => _ShoeCategoryState();
+}
+
+class _ShoeCategoryState extends State<ShoeCategory> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
