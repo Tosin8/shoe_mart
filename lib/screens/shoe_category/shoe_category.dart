d8676940aa7ae54a8d9327e17c09d3e7a2378ffd
Tosin Ezekiel <23019300+Tosin8@users.noreply.github.com>
2023-07-02 21:38:23 +0100

    Extract Widget for Product Category

--- lib/screens/shoe_category/shoe_category.dart
+++ lib/screens/shoe_category/shoe_category.dart
@@ -18,28 +18,50 @@ class _ShoeCategoryState extends State<ShoeCategory> {
               style: TextStyle(fontWeight: FontWeight.w400),
             ),
             backgroundColor: kPrimaryColor),
-        body: ListView(children: [
-          Container(
-            padding: const EdgeInsets.all(2),
-            height: 120,
-            child: Card(
-              child: Row(
-                children: [
-                  Image.asset('assets/products/shoe1.jpg'),
-                  Expanded(
-                      child: Column(
-                    children: [
-                      Text(
-                        'Nike Shoe 1',
-                        style: TextStyle(fontWeight: FontWeight.w600),
-                      ),
-                      Text('Price'),
-                    ],
-                  ))
-                ],
-              ),
-            ),
-          )
-        ]));
+        body: ListView(children: [Product_Details()]));
+  }
+}
+
+class Product_Details extends StatelessWidget {
+  const Product_Details({
+    super.key,
+    required this.name,
+    required this.image,
+    required this.price,
+    required this.rating,
+    required this.description,
+  });
+
+  final String name;
+  final String image;
+  final int price;
+  final int rating;
+  final String description;
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.all(2),
+      height: 120,
+      child: Card(
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            Image.asset('assets/products/shoe1.jpg'),
+            const Expanded(
+                child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                Text(
+                  'Nike Shoe 1',
+                  style: TextStyle(fontWeight: FontWeight.w600),
+                ),
+                Text('Made with thick fibre materials'),
+                Text('Price'),
+              ],
+            ))
+          ],
+        ),
+      ),
+    );
   }
 }
