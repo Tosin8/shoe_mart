c723a070bdb9bec657a713fa8bc59d7671b4fd05
Tosin Ezekiel <23019300+Tosin8@users.noreply.github.com>
2023-07-02 21:12:35 +0100

    Update shoe_category.dart

--- lib/screens/shoe_category/shoe_category.dart
+++ lib/screens/shoe_category/shoe_category.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:shoe_store/widgets/constants.dart';
 
 class ShoeCategory extends StatefulWidget {
   const ShoeCategory({super.key});
@@ -12,7 +13,7 @@ class _ShoeCategoryState extends State<ShoeCategory> {
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(
-            title: const Text('Shoe Catalog'), backgroundColor: Colors.yellow),
+            title: const Text('Shoe Catalog'), backgroundColor: kPrimaryColor),
         body: ListView(children: [
           Text('Shoe Category'),
         ]));
