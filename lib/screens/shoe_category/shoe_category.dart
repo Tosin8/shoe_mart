f1d5635a9a22538f2d0485ef28dfdb363a40dee7
Tosin Ezekiel <23019300+Tosin8@users.noreply.github.com>
2023-07-05 14:27:25 +0100

    Update shoe_category.dart

--- lib/screens/shoe_category/shoe_category.dart
+++ lib/screens/shoe_category/shoe_category.dart
@@ -3,7 +3,6 @@ import 'package:flutter/services.dart';
 import 'package:shoe_store/widgets/constants.dart';
 import 'package:shoe_store/widgets/navigation_bar.dart';
 
-
 class ShoeCategory extends StatefulWidget {
   const ShoeCategory({super.key});
 
@@ -19,88 +18,114 @@ class _ShoeCategoryState extends State<ShoeCategory> {
     return Scaffold(
         // writing the navigation bar
         bottomNavigationBar: Container(
-          margin: EdgeInsets.all(displayWidth * .05),
-          height: displayWidth * .155,
-          decoration: BoxDecoration(
-            color: Colors.white,
-            boxShadow: [
-              BoxShadow(
-                color: Colors.black.withOpacity(.1),
-                blurRadius: 30,
-                offset: const Offset(0, 10),
-                spreadRadius: 1,
-              ),
-            ],
-            borderRadius: BorderRadius.circular(50),
-          ),
-          child: ListView.builder(
+            margin: EdgeInsets.all(displayWidth * .05),
+            height: displayWidth * .155,
+            decoration: BoxDecoration(
+              color: Colors.white,
+              boxShadow: [
+                BoxShadow(
+                  color: Colors.black.withOpacity(.1),
+                  blurRadius: 30,
+                  offset: const Offset(0, 10),
+                  spreadRadius: 1,
+                ),
+              ],
+              borderRadius: BorderRadius.circular(50),
+            ),
+            child: ListView.builder(
               itemCount: 4,
               scrollDirection: Axis.horizontal,
               padding: EdgeInsets.symmetric(horizontal: displayWidth * .02),
               itemBuilder: (context, index) => InkWell(
-                    onTap: () {
-                      setState(() {
-                        currentIndex = index;
-                        HapticFeedback.lightImpact();
-                      });
-                    },
-                    splashColor: Colors.transparent,
-                    highlightColor: Colors.transparent,
-                    child: Stack(
-                      children: [
-                        AnimatedContainer(
+                  onTap: () {
+                    setState(() {
+                      currentIndex = index;
+                      HapticFeedback.lightImpact();
+                    });
+                  },
+                  splashColor: Colors.transparent,
+                  highlightColor: Colors.transparent,
+                  child: Stack(
+                    children: [
+                      AnimatedContainer(
+                        duration: const Duration(seconds: 1),
+                        curve: Curves.fastLinearToSlowEaseIn,
+                        width: index == currentIndex
+                            ? displayWidth * .32
+                            : displayWidth * .18,
+                        alignment: Alignment.center,
+                        child: AnimatedContainer(
+                          duration: const Duration(seconds: 1),
+                          curve: Curves.fastLinearToSlowEaseIn,
+                          height:
+                              index == currentIndex ? displayWidth * .12 : 0,
+                          width: index == currentIndex ? displayWidth * .32 : 0,
+                          decoration: BoxDecoration(
+                            color: index == currentIndex
+                                ? Colors.blueAccent.withOpacity(.2)
+                                : Colors.transparent,
+                            borderRadius: BorderRadius.circular(50),
+                          ),
+                        ),
+                      ),
+                      AnimatedContainer(
                           duration: const Duration(seconds: 1),
                           curve: Curves.fastLinearToSlowEaseIn,
                           width: index == currentIndex
-                              ? displayWidth * .32
+                              ? displayWidth * .31
                               : displayWidth * .18,
                           alignment: Alignment.center,
-                          child: AnimatedContainer(duration: const Duration(seconds: 1), 
-                          curve: Curves.fastLinearToSlowEaseIn,
-                          height: index == currentIndex ? displayWidth * .12 : 0, 
-                          width: index == currentIndex ? displayWidth * .32 : 0,
-                          decoration:  BoxDecoration(
-                            color: index == currentIndex ? Colors.blueAccent.withOpacity(.2) : Colors.transparent, borderRadius: BorderRadius.circular(50), 
-                          ),),
-                        ), 
-       AnimatedContainer(
-        duration: const Duration(seconds: 1),
-        curve: Curves.fastLinearToSlowEaseIn,
-      width: index == currentIndex
-     ? displayWidth * .31 : displayWidth * .18, 
-      alignment: Alignment.center, 
-      child: Stack(
-        children: [
-          Row(
-            children: [
-              AnimatedContainer(duration: const Duration(seconds: 1), 
-              curve: Curves.fastLinearToSlowEaseIn, width: index == currentIndex ? displayWidth * .13 : 0,), AnimatedOpacity(opacity: index == currentIndex ? 1 : 0, duration: const Duration(seconds: 1), curve: Curves.fastLinearToSlowEaseIn,
-              child: Text(index == currentIndex ?  '\u0024{listOfStrings[index]}' : '',
-              style: const TextStyle(color: Colors.blueAccent, fontWeight: FontWeight.w600, fontSize: 15) ),)
-            ],
-          )
-        ],
-      ),
-      Row(
-        children: [
-        AnimatedContainer(
-          duration: const Duration(seconds:1), 
-        curve: Curves.fastLinearToSlowEaseIn,
-        width: index == currentIndex ? displayWidth * .03 : 20,
-        ), 
-        Icon(listofIcons[index],
-        size: displayWidth * .076,
-        color: index == currentIndex ? Colors.blueAccent: Colors.black26, 
-        ),
-      ],
-      ), 
-                      ], 
-       )
-                      
-                    ),
-                    
+                          child: Stack(
+                            children: [
+                              Row(
+                                children: [
+                                  AnimatedContainer(
+                                    duration: const Duration(seconds: 1),
+                                    curve: Curves.fastLinearToSlowEaseIn,
+                                    width: index == currentIndex
+                                        ? displayWidth * .13
+                                        : 0,
+                                  ),
+                                  AnimatedOpacity(
+                                    opacity: index == currentIndex ? 1 : 0,
+                                    duration: const Duration(seconds: 1),
+                                    curve: Curves.fastLinearToSlowEaseIn,
+                                    child: Text(
+                                      index == currentIndex
+                                          ? '\u0024{listOfStrings[index]}'
+                                          : '',
+                                      style: const TextStyle(
+                                        color: Colors.blueAccent,
+                                        fontWeight: FontWeight.w600,
+                                        fontSize: 15,
+                                      ),
+                                    ),
+                                  ),
+                                ],
+                              ),
+                              Row(
+                                children: [
+                                  AnimatedContainer(
+                                    duration: const Duration(seconds: 1),
+                                    curve: Curves.fastLinearToSlowEaseIn,
+                                    width: index == currentIndex
+                                        ? displayWidth * .03
+                                        : 20,
+                                  ),
+                                  Icon(
+                                    listofIcons[index],
+                                    size: displayWidth * .076,
+                                    color: index == currentIndex
+                                        ? Colors.blueAccent
+                                        : Colors.black26,
+                                  ),
+                                ],
+                              ),
+                            ],
+                          )),
+                    ],
                   )),
-        
+            )),
         appBar: AppBar(
             title: const Text(
               'Shoe Catalog',
@@ -116,6 +141,20 @@ class _ShoeCategoryState extends State<ShoeCategory> {
           ),
         ]));
   }
+
+  List<IconData> listofIcons = [
+    Icons.home_rounded,
+    Icons.favorite_rounded,
+    Icons.settings_rounded,
+    Icons.person_rounded,
+  ];
+
+  List<String> listOfStrings = [
+    'Home',
+    'Favorite',
+    'Settings',
+    'Profile',
+  ];
 }
 
 // ignore: camel_case_types
