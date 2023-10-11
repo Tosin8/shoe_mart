4dd6749d8ebd6f39b5913416386fd1d39b54c65f
Tosin Ezekiel <23019300+Tosin8@users.noreply.github.com>
2023-10-11 17:25:32 +0100

    Refactoring Code

--- lib/widgets/navigation_bar.dart
+++ lib/widgets/navigation_bar.dart
@@ -1,15 +1,1 @@
-import 'package:flutter/material.dart';
 
-List<IconData> listofIcons = [
-  Icons.home_rounded,
-  Icons.favorite_rounded,
-  Icons.settings_rounded,
-  Icons.person_rounded,
-];
-
-List<String> listOfStrings = [
-  'Home',
-  'Favorite',
-  'Settings',
-  'Profile',
-];
