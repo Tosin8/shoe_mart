a64b2d7c9f7d31ec7a307ddef5fe74805a21b15b
Tosin Ezekiel <23019300+Tosin8@users.noreply.github.com>
2023-10-12 15:49:17 +0100

    Creating Splash Screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:shoe_store/splash/splash.dart';
 
 import 'screens/home/home.dart';
 import 'screens/shoe_category/shoe_category.dart';
@@ -20,7 +21,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const Home(),
+      home: const SplashScreen(),
     );
   }
 }
---
+++ lib/splash/splash.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({super.key});
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+          decoration: BoxDecoration(
+              image: DecorationImage(
+                  image: AssetImage('assets/splash_image.jpg'),
+                  fit: BoxFit.cover))),
+    );
+  }
+}
