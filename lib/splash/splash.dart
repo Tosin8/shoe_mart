e68c6a9df73866972a7a83242bdf4f657e68ca7b
Tosin Ezekiel <23019300+Tosin8@users.noreply.github.com>
2023-10-13 11:41:11 +0100

    Refactoring

--- lib/splash/splash.dart
+++ lib/splash/splash.dart
@@ -1,7 +1,5 @@
 import 'package:flutter/material.dart';
 
-import 'splash_widget.dart';
-
 class SplashScreen extends StatefulWidget {
   const SplashScreen({super.key});
 
@@ -9,86 +7,9 @@ class SplashScreen extends StatefulWidget {
   State<SplashScreen> createState() => _SplashScreenState();
 }
 
-class _SplashScreenState extends State<SplashScreen>
-    with TickerProviderStateMixin {
-  late AnimationController _scaleController;
-  late Animation<double> _scaleAnimation;
-
-  bool hide = false;
-
-  @override
-  void initState() {
-    // TODO: implement initState
-    super.initState();
-
-    _scaleController =
-        AnimationController(vsync: this, duration: Duration(milliseconds: 500));
-
-    _scaleAnimation = Tween<double>(begin: 1.0, end: 30.0);
-  }
-
+class _SplashScreenState extends State<SplashScreen> {
   @override
   Widget build(BuildContext context) {
-    return SafeArea(
-      child: Scaffold(
-        body: Container(
-            width: double.infinity,
-            decoration: const BoxDecoration(
-              image: DecorationImage(
-                  image: AssetImage('assets/splash_image.jpg'),
-                  fit: BoxFit.cover),
-            ),
-            child: Container(
-              decoration: BoxDecoration(
-                  gradient: LinearGradient(colors: [
-                Colors.black.withOpacity(.9),
-                Colors.black.withOpacity(.4),
-              ], begin: Alignment.bottomRight)),
-              child: const Stack(children: [
-                Positioned(
-                  top: 15,
-                  left: 15,
-                  child: Text(
-                    'Shoe Mart',
-                    style: TextStyle(
-                        color: Colors.white,
-                        fontSize: 14,
-                        fontWeight: FontWeight.w300),
-                  ),
-                ),
-                Padding(
-                  padding: EdgeInsets.all(50.0),
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.center,
-                    mainAxisAlignment: MainAxisAlignment.end,
-                    children: [
-                      Text(
-                        'Find your best collections',
-                        textAlign: TextAlign.center,
-                        style: TextStyle(
-                          color: Colors.white,
-                          fontWeight: FontWeight.bold,
-                          fontSize: 30,
-                        ),
-                      ),
-                      SizedBox(height: 12),
-                      Text(
-                        'Brand New Perspective',
-                        style: TextStyle(
-                            color: Colors.white,
-                            fontWeight: FontWeight.w300,
-                            fontSize: 18),
-                      ),
-                      SizedBox(height: 12),
-                      BtnStart(),
-                      SizedBox(height: 15),
-                      BtnLogin(),
-                    ],
-                  ),
-                ),
-              ]),
-            )),
-      ),
-    );
+    return const Placeholder();
   }
 }
