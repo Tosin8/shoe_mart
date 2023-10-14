fad6efdae4e5c5e70bc678d1d8b78590d4c913c3
Tosin Ezekiel <23019300+Tosin8@users.noreply.github.com>
2023-10-14 04:28:16 +0100

    Update home.dart

--- lib/screens/home/home.dart
+++ lib/screens/home/home.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:shoe_store/extensions/constants.dart';
 
 class Home extends StatefulWidget {
   const Home({super.key});
@@ -14,13 +15,45 @@ class _HomeState extends State<Home> {
         appBar: AppBar(
           // elevation: 1,
           centerTitle: true,
-          title: Text('Home'),
-          automaticallyImplyLeading: false, // remove back button in appbar.
+          title: const Text(
+            'Home',
+            style: TextStyle(color: Colors.black),
+          ),
+          automaticallyImplyLeading: false,
+          actions: [
+            IconButton(onPressed: () {}, icon: const Icon(Icons.notifications)),
+          ], // remove back button in appbar.
         ),
-        body: ListView(
-          children: [
-            
-          ],
+        body: Padding(
+          padding: const EdgeInsets.all(10.0),
+          child: ListView(
+            children: [
+              Text(
+                'Shoes Collections',
+                style: TextStyle(
+                    fontSize: 18,
+                    fontWeight: FontWeight.bold,
+                    color: kAccentColor),
+              ),
+              Text('The best of shoes in one place!',
+                  style: TextStyle(color: kAccentColor)),
+              SizedBox(height: 10),
+              Container(
+                height: 50,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(30),
+                  color: Colors.white,
+                  boxShadow: [
+                    BoxShadow(
+                        color: Colors.black.withOpacity(0.3),
+                        blurRadius: 10,
+                        offset: Offset(2, 2))
+                  ],
+                ),
+                child: TextField(),
+              )
+            ],
+          ),
         ));
   }
 }
