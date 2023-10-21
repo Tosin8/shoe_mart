7ded9a2f1908040435a08ed5bfcdf8dda1c54483
Tosin Ezekiel <23019300+Tosin8@users.noreply.github.com>
2023-10-21 15:34:26 +0100

    Update splash.dart

--- lib/extensions/splash.dart
+++ lib/extensions/splash.dart
@@ -50,143 +50,140 @@ class _SplashScreenState extends State<SplashScreen>
   Widget build(BuildContext context) {
     return SafeArea(
       child: Scaffold(
-          body: Transform(
-            
-            child: Container(
-                height: MediaQuery.of(context).size.height,
-                width: double.infinity,
-                decoration: const BoxDecoration(
-                    image: DecorationImage(
-                  image: AssetImage('assets/image/splash_image.jpg'),
-                  fit: BoxFit.cover,
-                )),
-                child: Stack(
-                  children: [
-                    Container(
-                      decoration: BoxDecoration(
-                          gradient: LinearGradient(colors: [
-                        Colors.black.withOpacity(.9),
-                        Colors.black.withOpacity(.4)
-                      ], begin: Alignment.bottomRight)),
+          body: Container(
+              height: MediaQuery.of(context).size.height,
+              width: double.infinity,
+              decoration: const BoxDecoration(
+                  image: DecorationImage(
+                image: AssetImage('assets/image/splash_image.jpg'),
+                fit: BoxFit.cover,
+              )),
+              child: Stack(
+                children: [
+                  Container(
+                    decoration: BoxDecoration(
+                        gradient: LinearGradient(colors: [
+                      Colors.black.withOpacity(.9),
+                      Colors.black.withOpacity(.4)
+                    ], begin: Alignment.bottomRight)),
+                  ),
+                  const Positioned(
+                    top: 25,
+                    left: 10,
+                    child: Text(
+                      'Shoe Mart',
+                      style: TextStyle(
+                          color: Colors.white,
+                          fontWeight: FontWeight.w600,
+                          fontSize: 16),
                     ),
-                    const Positioned(
-                      top: 25,
-                      left: 10,
-                      child: Text(
-                        'Shoe Mart',
-                        style: TextStyle(
-                            color: Colors.white,
-                            fontWeight: FontWeight.w600,
-                            fontSize: 16),
-                      ),
-                    ),
-                    Positioned(
-                      top: 10,
-                      right: 10,
-                      child: IconButton(
-                          onPressed: () {
-                            closeAppUsingExit();
+                  ),
+                  Positioned(
+                    top: 10,
+                    right: 10,
+                    child: IconButton(
+                        onPressed: () {
+                          closeAppUsingExit();
+                        },
+                        icon: const Icon(Icons.power_settings_new,
+                            color: Colors.white)),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(80.0),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      mainAxisAlignment: MainAxisAlignment.end,
+                      children: [
+                        const Text(
+                          'Find your best collections',
+                          textAlign: TextAlign.center,
+                          style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 20,
+                              fontWeight: FontWeight.bold),
+                        ),
+                        const SizedBox(height: 10),
+                        AnimatedTextKit(
+                          animatedTexts: [
+                            TypewriterAnimatedText('Brand New Perspective',
+                                textStyle: const TextStyle(
+                                    color: Colors.white,
+                                    fontWeight: FontWeight.w300),
+                                speed: const Duration(milliseconds: 300),
+                                textAlign: TextAlign.center),
+                          ],
+                          //  totalRepeatCount: 8,
+                          pause: const Duration(milliseconds: 500),
+                          displayFullTextOnTap: true,
+                          stopPauseOnTap: true,
+                          repeatForever: true,
+                        ),
+                        const SizedBox(height: 10),
+                        InkWell(
+                          onTap: () {
+                            setState(() {
+                              hide = true;
+                            });
+                            _scaleController.forward();
                           },
-                          icon: const Icon(Icons.power_settings_new,
-                              color: Colors.white)),
-                    ),
-                    Padding(
-                      padding: const EdgeInsets.all(80.0),
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.center,
-                        mainAxisAlignment: MainAxisAlignment.end,
-                        children: [
-                          const Text(
-                            'Find your best collections',
-                            textAlign: TextAlign.center,
-                            style: TextStyle(
-                                color: Colors.white,
-                                fontSize: 20,
-                                fontWeight: FontWeight.bold),
-                          ),
-                          const SizedBox(height: 10),
-                          AnimatedTextKit(
-                            animatedTexts: [
-                              TypewriterAnimatedText('Brand New Perspective',
-                                  textStyle: const TextStyle(
-                                      color: Colors.white,
-                                      fontWeight: FontWeight.w300),
-                                  speed: const Duration(milliseconds: 300),
-                                  textAlign: TextAlign.center),
-                            ],
-                            //  totalRepeatCount: 8,
-                            pause: const Duration(milliseconds: 500),
-                            displayFullTextOnTap: true,
-                            stopPauseOnTap: true,
-                            repeatForever: true,
-                          ),
-                          const SizedBox(height: 10),
-                          InkWell(
-                            onTap: () {
-                              setState(() {
-                                hide = true;
-                              });
-                              _scaleController.forward();
-                            },
-                            child: AnimatedBuilder(
-                              animation: _scaleController,
-                              builder: (context, child) => Transform.scale(
-                                scale: _scaleAnimation.value,
-                                child: Container(
-                                    height: 50,
-                                    decoration: BoxDecoration(
-                                        borderRadius: BorderRadius.circular(50),
-                                        color: Colors.white),
-                                    child: Center(
-                                      child: hide == false
-                                          ? const Text(
-                                              'Start Shopping ',
-                                              style: TextStyle(
-                                                  fontWeight: FontWeight.w500),
-                                            )
-                                          : Container(),
-                                    )),
-                              ),
+                          child: AnimatedBuilder(
+                            animation: _scaleController,
+                            builder: (context, child) => Transform.scale(
+                              scale: _scaleAnimation.value,
+                              child: Container(
+                                  height: 50,
+                                  decoration: BoxDecoration(
+                                      borderRadius: BorderRadius.circular(50),
+                                      color: Colors.white),
+                                  child: Center(
+                                    child: hide == false
+                                        ? const Text(
+                                            'Start Shopping ',
+                                            style: TextStyle(
+                                                fontWeight: FontWeight.w500),
+                                          )
+                                        : Container(),
+                                  )),
                             ),
                           ),
-                          const SizedBox(height: 10),
-                          InkWell(
-                            highlightColor: Colors.transparent,
-                            splashColor: Colors.transparent,
-                            onHighlightChanged: (value) {
-                              setState(() {
-                                isTapped = value;
-                              });
-                            },
-                            onTap: () {
-                              Navigator.push(
-                                  context,
-                                  MaterialPageRoute(
-                                      builder: (context) => const LogIn()));
-                            },
-                            child: AnimatedContainer(
-                                duration: const Duration(milliseconds: 300),
-                                curve: Curves.fastLinearToSlowEaseIn,
-                                height: isTapped ? 50 : 55,
-                                width: isTapped ? 200 : 250,
-                                decoration: BoxDecoration(
-                                    borderRadius: BorderRadius.circular(50),
-                                    border: Border.all(color: Colors.white)),
-                                child: const Center(
-                                  child: Text(
-                                    'Log In',
-                                    style: TextStyle(
-                                        fontWeight: FontWeight.w500,
-                                        color: Colors.white),
-                                  ),
-                                )),
-                          ),
-                        ],
-                      ),
-                    )
-                  ],
-                )),
-          )),
+                        ),
+                        const SizedBox(height: 10),
+                        InkWell(
+                          highlightColor: Colors.transparent,
+                          splashColor: Colors.transparent,
+                          onHighlightChanged: (value) {
+                            setState(() {
+                              isTapped = value;
+                            });
+                          },
+                          onTap: () {
+                            Navigator.push(
+                                context,
+                                MaterialPageRoute(
+                                    builder: (context) => const LogIn()));
+                          },
+                          child: AnimatedContainer(
+                              duration: const Duration(milliseconds: 300),
+                              curve: Curves.fastLinearToSlowEaseIn,
+                              height: isTapped ? 50 : 55,
+                              width: isTapped ? 200 : 250,
+                              decoration: BoxDecoration(
+                                  borderRadius: BorderRadius.circular(50),
+                                  border: Border.all(color: Colors.white)),
+                              child: const Center(
+                                child: Text(
+                                  'Log In',
+                                  style: TextStyle(
+                                      fontWeight: FontWeight.w500,
+                                      color: Colors.white),
+                                ),
+                              )),
+                        ),
+                      ],
+                    ),
+                  )
+                ],
+              ))),
     );
   }
 }
