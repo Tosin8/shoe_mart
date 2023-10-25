ae532ec1b4a85c5e7ec8403cc32ee3f0e4e9c61c
Tosin Ezekiel <23019300+Tosin8@users.noreply.github.com>
2023-10-25 16:14:50 +0100

    Refactoring Product Card

--- lib/screens/home/home_widget.dart
+++ lib/screens/home/home_widget.dart
@@ -236,128 +236,136 @@ class AdidasCard extends StatefulWidget {
 class _AdidasCardState extends State<AdidasCard> {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      width: 177,
-      height: 277,
-      child: Stack(
-        children: [
-          Positioned(
-            left: 0,
-            top: 0,
-            child: Container(
-              width: 177,
-              height: 277,
-              decoration: const BoxDecoration(color: Color(0x00D9D9D9)),
-            ),
-          ),
-          Positioned(
-            left: 0,
-            top: 0,
-            child: Container(
-              width: 174,
-              height: 154,
-              decoration: ShapeDecoration(
-                color: const Color(0xFFFCF7F7),
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(32),
-                ),
-              ),
-            ),
-          ),
-          Positioned(
-            left: 6,
-            top: 72.87,
-            child: Transform(
-              transform: Matrix4.identity()
-                ..translate(0.0, 0.0)
-                ..rotateZ(-0.55),
+    return GestureDetector(
+      onTap: () {},
+      child: Container(
+        width: 177,
+        height: 277,
+        child: Stack(
+          children: [
+            Positioned(
+              left: 0,
+              top: 0,
               child: Container(
-                width: 130,
-                height: 84,
-                decoration: BoxDecoration(
-                  image: const DecorationImage(
-                    image: AssetImage(
-                        "assets/products/devinbooker_basket_nike.png"),
-                    fit: BoxFit.fill,
+                width: 177,
+                height: 277,
+                decoration: const BoxDecoration(color: Color(0x00D9D9D9)),
+              ),
+            ),
+            Positioned(
+              left: 0,
+              top: 0,
+              child: Container(
+                width: 174,
+                height: 154,
+                decoration: ShapeDecoration(
+                  color: const Color(0xFFFCF7F7),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(32),
                   ),
-                  boxShadow: [
-                    const BoxShadow(
-                            color: Color.fromARGB(255, 156, 153, 153),
-                            spreadRadius: 1,
-                            blurRadius: 25,
-                            offset: Offset(0, 10))
-                        .scale(3),
-                  ],
                 ),
               ),
             ),
-          ),
-          const Positioned(
-            left: 16,
-            top: 170,
-            child: SizedBox(
-              width: 63,
-              child: Text(
-                '\$129.00',
-                style: TextStyle(
-                  color: Colors.black,
-                  fontSize: 16,
-                  fontFamily: 'Inter',
-                  fontWeight: FontWeight.w500,
-                  height: 0,
+            Positioned(
+              left: 6,
+              top: 72.87,
+              child: Transform(
+                transform: Matrix4.identity()
+                  ..translate(0.0, 0.0)
+                  ..rotateZ(-0.55),
+                child: Container(
+                  width: 130,
+                  height: 84,
+                  decoration: BoxDecoration(
+                    image: const DecorationImage(
+                      image: AssetImage(
+                          "assets/products/devinbooker_basket_nike.png"),
+                      fit: BoxFit.fill,
+                    ),
+                    boxShadow: [
+                      const BoxShadow(
+                              color: Color.fromARGB(255, 156, 153, 153),
+                              spreadRadius: 1,
+                              blurRadius: 25,
+                              offset: Offset(0, 10))
+                          .scale(3),
+                    ],
+                  ),
                 ),
               ),
             ),
-          ),
-          const Positioned(
-            left: 16,
-            top: 191,
-            child: SizedBox(
-              width: 93,
-              child: Text(
-                'Nike Lebron 8',
-                style: TextStyle(
-                  color: Colors.black,
-                  fontSize: 14,
-                  fontFamily: 'Inter',
-                  fontWeight: FontWeight.w400,
-                  height: 0,
+            const Positioned(
+              left: 16,
+              top: 170,
+              child: SizedBox(
+                width: 63,
+                child: Text(
+                  '\$129.00',
+                  style: TextStyle(
+                    color: Colors.black,
+                    fontSize: 16,
+                    fontFamily: 'Inter',
+                    fontWeight: FontWeight.w500,
+                    height: 0,
+                  ),
                 ),
               ),
             ),
-          ),
-          const Positioned(
-            left: 108,
-            top: 165,
-            child: SizedBox(width: 18, height: 12, child: Icon(Icons.star)),
-          ),
-          const Positioned(
-            left: 132,
-            top: 170,
-            child: SizedBox(
-              width: 21,
-              child: Text(
-                '4.8',
-                style: TextStyle(
-                  color: Colors.black,
-                  fontSize: 14,
-                  fontFamily: 'Inter',
-                  fontWeight: FontWeight.w300,
-                  height: 0,
+            const Positioned(
+              left: 16,
+              top: 191,
+              child: SizedBox(
+                width: 93,
+                child: Text(
+                  'Nike Lebron 8',
+                  style: TextStyle(
+                    color: Colors.black,
+                    fontSize: 14,
+                    fontFamily: 'Inter',
+                    fontWeight: FontWeight.w400,
+                    height: 0,
+                  ),
                 ),
               ),
             ),
-          ),
-          Positioned(
-            left: 25,
-            top: 20,
-            child: Container(
-              width: 17,
-              height: 17,
-              child: const Icon(Icons.favorite_outlined),
+            const Positioned(
+              left: 108,
+              top: 165,
+              child: SizedBox(
+                  width: 18,
+                  height: 12,
+                  child: Icon(Icons.star, color: Colors.yellowAccent)),
             ),
-          ),
-        ],
+            const Positioned(
+              left: 132,
+              top: 170,
+              child: SizedBox(
+                width: 21,
+                child: Text(
+                  '4.8',
+                  style: TextStyle(
+                    color: Colors.black,
+                    fontSize: 14,
+                    fontFamily: 'Inter',
+                    fontWeight: FontWeight.w300,
+                    height: 0,
+                  ),
+                ),
+              ),
+            ),
+            Positioned(
+              left: 25,
+              top: 20,
+              child: Container(
+                width: 17,
+                height: 17,
+                child: const Icon(
+                  Icons.favorite_border_outlined,
+                ),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
