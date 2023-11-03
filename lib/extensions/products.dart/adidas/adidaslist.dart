6602bff805dec7a8c7f66411fc73ebbe4d683973
Tosin Ezekiel <23019300+Tosin8@users.noreply.github.com>
2023-11-03 14:35:57 +0100

    Updating

--- lib/extensions/products.dart/adidas/adidaslist.dart
+++ lib/extensions/products.dart/adidas/adidaslist.dart
@@ -1,12 +1,9 @@
 // class AdidasList {
 //   static List<Product> adidaslistItem = [
 //     Product(
-//       name: 'Nike Air Max 2000', 
-//       price: 180.00, 
-//       image: 
-//     );
+//       name: 'Nike Air Max 2000',
+//       price: 180.00,
+//       image: '',
+//     ),
 //   ];
-// }
-
-// class Product {
 // }
--- lib/screens/home/home.dart
+++ lib/screens/home/home.dart
@@ -13,168 +13,145 @@ class Home extends StatefulWidget {
 
 class _HomeState extends State<Home> {
   final TextEditingController _searchController = TextEditingController();
+  final List<String> _adidaslistItem = [
+    'assets/products/adidas/1.jpg',
+    'assets/products/adidas/2.jpg',
+    'assets/products/adidas/3.jpg',
+    'assets/products/adidas/4.jpg',
+    'assets/products/adidas/5.jpg',
+    'assets/products/adidas/6.jpg'
+  ];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          // elevation: 1,
-          centerTitle: true,
-          title: const Text(
-            'Home',
-            style: TextStyle(color: Colors.black),
-          ),
-          automaticallyImplyLeading: false,
-          actions: [
-            Stack(children: [
-              Positioned(
-                top: 10,
-                left: 8,
-                child: Container(
-                  height: 30,
-                  width: 30,
-                  decoration: BoxDecoration(
-                    color: Colors.white.withOpacity(0.8),
-                    shape: BoxShape.circle,
-                    boxShadow: [
-                      BoxShadow(
-                          color: Colors.black.withOpacity(0.3),
-                          blurRadius: 10,
-                          offset: const Offset(2, 2))
-                    ],
-                  ),
+      appBar: AppBar(
+        // elevation: 1,
+        centerTitle: true,
+        title: const Text(
+          'Home',
+          style: TextStyle(color: Colors.black),
+        ),
+        automaticallyImplyLeading: false,
+        actions: [
+          Stack(children: [
+            Positioned(
+              top: 10,
+              left: 8,
+              child: Container(
+                height: 30,
+                width: 30,
+                decoration: BoxDecoration(
+                  color: Colors.white.withOpacity(0.8),
+                  shape: BoxShape.circle,
+                  boxShadow: [
+                    BoxShadow(
+                        color: Colors.black.withOpacity(0.3),
+                        blurRadius: 10,
+                        offset: const Offset(2, 2))
+                  ],
                 ),
               ),
-              IconButton(
-                  onPressed: () {},
-                  icon: SvgPicture.asset('assets/icons/bell.svg'))
-            ]),
-          ], // remove back button in appbar.
-        ),
-        body: ListView(padding: const EdgeInsets.all(10.0), children: [
-          Column(children: [
-            const Text(
-              'Shoes Collections',
-              style: TextStyle(
-                  fontSize: 22,
-                  fontWeight: FontWeight.bold,
-                  color: kAccentColor),
             ),
-            const Text('The best of shoes in one place!',
-                style: TextStyle(color: kAccentColor, fontSize: 16)),
-            const SizedBox(height: 10),
-            searchBtn(searchController: _searchController),
-            const SizedBox(height: 10),
-
-            advertBanner(),
-            const SizedBox(height: 15),
-
-            // Creating categories.
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                const Text(
-                  'Recommended',
-                  style: TextStyle(
-                      color: Colors.black,
-                      fontSize: 16,
-                      fontWeight: FontWeight.bold),
-                ),
-                GestureDetector(
-                  onTap: () {},
-                  child: const Text(
-                    'See all',
-                    style: TextStyle(color: kSecondaryColor),
-                  ),
-                ),
-              ],
-            ),
-            const SizedBox(height: 6),
-            SizedBox(
-              height: 350,
-              child:
-                  ListView(scrollDirection: Axis.horizontal, children: const [
-                ProductCard(
-                  productname: "Nike Zoom Pegasus",
-                  categoryname: "Men's Shoe",
-                  categorycolor: "3 Colors",
-                  image: "assets/products/zoom_pegasus/1.png",
-                  price: '\$80.00',
-                ),
-                ProductCard(
-                  productname: "Nike Airforce",
-                  categoryname: "Men's Shoe",
-                  categorycolor: "2 Colors",
-                  image: "assets/products/airforce/1.png",
-                  price: '\$60.00',
-                ),
-                ProductCard(
-                  productname: "Nike Airforce",
-                  categoryname: "Men's Shoe",
-                  categorycolor: "2 Colors",
-                  image: "assets/products/airforce/1.png",
-                  price: '\$36.00',
-                ),
-                ProductCard(
-                  productname: "Nike Airforce",
-                  categoryname: "Men's Shoe",
-                  categorycolor: "2 Colors",
-                  image: "assets/products/airforce/1.png",
-                  price: '\$58.00',
-                ),
-                ProductCard(
-                  productname: "Nike Airforce",
-                  categoryname: "Men's Shoe",
-                  categorycolor: "2 Colors",
-                  image: "assets/products/airforce/1.png",
-                  price: '\$61.00',
-                ),
-              ]),
-            ),
-
-            const SizedBox(height: 6),
-            Row(
-              children: [
-                Image.asset('assets/icons/brands/adidas.png'),
-                const SizedBox(width: 5),
-                const Text(
-                  'Adidas',
-                  style: TextStyle(
-                    fontWeight: FontWeight.w500,
-                  ),
-                ),
-              ],
-            ),
-            // adidas product grid.
-            const SizedBox(height: 15),
-            // Expanded(
-            //     child: GridView.count(
-            //   crossAxisCount: 2,
-            //   mainAxisSpacing: 10,
-            //   crossAxisSpacing: 10,
-            //   padding: const EdgeInsets.all(8),
-            //   children: _adidaslistItem
-            //       .map((item) => Card(
-            //             color: Colors.transparent,
-            //             elevation: 0,
-            //             child: Container(
-            //               decoration: BoxDecoration(
-            //                   borderRadius: BorderRadius.circular(20),
-            //                   image: DecorationImage(
-            //                       image: AssetImage(item), fit: BoxFit.cover)),
-            //             ),
-            //           ))
-            //       .toList(),
-            // ))
-            Expanded(
-                child: GridView.builder(
-                    gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                        crossAxisCount: 2,
-                        childAspectRatio: (100 / 140),
-                        crossAxisSpacing: 12,
-                        mainAxisSpacing: 12),
-                    itemBuilder: itemBuilder))
+            IconButton(
+                onPressed: () {},
+                icon: SvgPicture.asset('assets/icons/bell.svg'))
           ]),
-        ]));
+        ], // remove back button in appbar.
+      ),
+      body: Column(children: [
+        const Text(
+          'Shoes Collections',
+          style: TextStyle(
+              fontSize: 22, fontWeight: FontWeight.bold, color: kAccentColor),
+        ),
+        const Text('The best of shoes in one place!',
+            style: TextStyle(color: kAccentColor, fontSize: 16)),
+        const SizedBox(height: 10),
+        searchBtn(searchController: _searchController),
+        const SizedBox(height: 10),
+
+        advertBanner(),
+        const SizedBox(height: 15),
+
+        // Creating categories.
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            const Text(
+              'Recommended',
+              style: TextStyle(
+                  color: Colors.black,
+                  fontSize: 16,
+                  fontWeight: FontWeight.bold),
+            ),
+            GestureDetector(
+              onTap: () {},
+              child: const Text(
+                'See all',
+                style: TextStyle(color: kSecondaryColor),
+              ),
+            ),
+          ],
+        ),
+        const SizedBox(height: 6),
+        SizedBox(
+          height: 350,
+          child: ListView(scrollDirection: Axis.horizontal, children: const [
+            ProductCard(
+              productname: "Nike Zoom Pegasus",
+              categoryname: "Men's Shoe",
+              categorycolor: "3 Colors",
+              image: "assets/products/zoom_pegasus/1.png",
+              price: '\$80.00',
+            ),
+            ProductCard(
+              productname: "Nike Airforce",
+              categoryname: "Men's Shoe",
+              categorycolor: "2 Colors",
+              image: "assets/products/airforce/1.png",
+              price: '\$60.00',
+            ),
+            ProductCard(
+              productname: "Nike Airforce",
+              categoryname: "Men's Shoe",
+              categorycolor: "2 Colors",
+              image: "assets/products/airforce/1.png",
+              price: '\$36.00',
+            ),
+            ProductCard(
+              productname: "Nike Airforce",
+              categoryname: "Men's Shoe",
+              categorycolor: "2 Colors",
+              image: "assets/products/airforce/1.png",
+              price: '\$58.00',
+            ),
+            ProductCard(
+              productname: "Nike Airforce",
+              categoryname: "Men's Shoe",
+              categorycolor: "2 Colors",
+              image: "assets/products/airforce/1.png",
+              price: '\$61.00',
+            ),
+          ]),
+        ),
+
+        const SizedBox(height: 6),
+        Row(
+          children: [
+            Image.asset('assets/icons/brands/adidas.png'),
+            const SizedBox(width: 5),
+            const Text(
+              'Adidas',
+              style: TextStyle(
+                fontWeight: FontWeight.w500,
+              ),
+            ),
+          ],
+        ),
+        // adidas product grid.
+        const SizedBox(height: 15),
+      ]),
+    );
   }
 }
