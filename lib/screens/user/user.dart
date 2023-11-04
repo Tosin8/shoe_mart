c87780427df68bd969658f8b1e9119776c4fae72
Tosin Ezekiel <23019300+Tosin8@users.noreply.github.com>
2023-11-04 15:11:17 +0100

    Update user.dart

--- lib/screens/user/user.dart
+++ lib/screens/user/user.dart
@@ -5,6 +5,6 @@ class User extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(body: Text('This is User Page'));
+    return Scaffold(body: Center(child: Text('This is User Page')));
   }
 }
